3f90c7176ca2977861b283829e21b74d0180a9b5
sejong <snm40109@gmail.com>
2024-08-21 17:25:16 +0900

    🐛 when app window small (compact x compact) and at trips/profile/more screen, fix that left padding exits

--- app/src/main/java/com/newpaper/somewhere/navigation/more/MoreNavigation.kt
+++ app/src/main/java/com/newpaper/somewhere/navigation/more/MoreNavigation.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
@@ -107,8 +108,10 @@ fun NavGraphBuilder.moreScreen(
             Row(
                 modifier = Modifier.weight(1f)
             ) {
-
-                if (
+                if (widthSizeClass == WindowWidthSizeClass.Compact){
+                    MySpacerRow(width = 0.dp)
+                }
+                else if (
                     heightSizeClass == WindowHeightSizeClass.Compact
                     || widthSizeClass == WindowWidthSizeClass.Medium
                 ) {
