b7b7a8bf24cc2924e7fa5f07e33111da19eb4422
sejong <snm40109@gmail.com>
2024-09-03 16:06:17 +0900

    ♻️ delete unused imports

--- core/ui/designsystem/src/main/java/com/newpaper/somewhere/core/designsystem/component/topAppBars/SomewhereTopAppBar.kt
+++ core/ui/designsystem/src/main/java/com/newpaper/somewhere/core/designsystem/component/topAppBars/SomewhereTopAppBar.kt
@@ -28,9 +28,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.newpaper.somewhere.core.designsystem.component.utils.MyPlainTooltipBox
 import com.newpaper.somewhere.core.designsystem.component.utils.MySpacerColumn
 import com.newpaper.somewhere.core.designsystem.component.utils.MySpacerRow
-import com.newpaper.somewhere.core.designsystem.component.utils.MyPlainTooltipBox
 import com.newpaper.somewhere.core.designsystem.icon.DisplayIcon
 import com.newpaper.somewhere.core.designsystem.icon.IconTextButtonIcon
 import com.newpaper.somewhere.core.designsystem.icon.MyIcon
--- core/ui/designsystem/src/main/java/com/newpaper/somewhere/core/designsystem/component/utils/MyPlainTooltipBox.kt
+++ core/ui/designsystem/src/main/java/com/newpaper/somewhere/core/designsystem/component/utils/MyPlainTooltipBox.kt
@@ -1,9 +1,5 @@
 package com.newpaper.somewhere.core.designsystem.component.utils
 
-import android.util.Log
-import androidx.compose.foundation.hoverable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsHoveredAsState
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -13,14 +9,8 @@ import androidx.compose.material3.TooltipBox
 import androidx.compose.material3.TooltipDefaults
 import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.PointerEventType
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
