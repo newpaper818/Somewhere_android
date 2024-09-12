e9ebe0edfcd8a9b26aa1a68d99b825f755a337c7
sejong <snm40109@gmail.com>
2024-09-12 13:48:44 +0900

    💄 fix max width fix - LoadingTripsItem

--- feature/trip/src/main/java/com/newpaper/somewhere/feature/trip/trips/component/LoadingTripsItem.kt
+++ feature/trip/src/main/java/com/newpaper/somewhere/feature/trip/trips/component/LoadingTripsItem.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,12 +16,12 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.newpaper.somewhere.core.designsystem.component.shimmerEffect
 import com.newpaper.somewhere.core.designsystem.component.utils.MySpacerColumn
+import com.newpaper.somewhere.core.utils.itemMaxWidth
 import com.newpaper.somewhere.feature.trip.trips.tripCardHeightDp
 
 @Composable
 internal fun LoadingTripsItem(
     shown: Boolean,
-    internetEnabled: Boolean,
     modifier: Modifier = Modifier
 ){
     AnimatedVisibility(
@@ -29,7 +30,7 @@ internal fun LoadingTripsItem(
         exit = fadeOut(animationSpec = tween(400))
     ) {
 
-        Column(modifier = modifier) {
+        Column(modifier = modifier.widthIn(max = itemMaxWidth)) {
 
             MySpacerColumn(height = 124.dp)
 
