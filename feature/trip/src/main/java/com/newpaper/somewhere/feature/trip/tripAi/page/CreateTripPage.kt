ea9f835fbf25aa667a700d6fa013ba793624f27d
sejong <snm40109@gmail.com>
2024-10-10 17:41:35 +0900

    ♻️ CreateTripPage string fix

--- feature/trip/src/main/java/com/newpaper/somewhere/feature/trip/tripAi/page/CreateTripPage.kt
+++ feature/trip/src/main/java/com/newpaper/somewhere/feature/trip/tripAi/page/CreateTripPage.kt
@@ -8,9 +8,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.newpaper.somewhere.core.designsystem.component.utils.MySpacerColumn
+import com.newpaper.somewhere.feature.trip.R
 
 @Composable
 internal fun CreateTripPage(
@@ -24,7 +26,7 @@ internal fun CreateTripPage(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = "Creating a trip with AI...",
+                text = stringResource(id = R.string.creating_trip_with_ai),
                 style = MaterialTheme.typography.displayLarge.copy(
                     fontWeight = FontWeight.Normal
                 )
@@ -33,7 +35,8 @@ internal fun CreateTripPage(
             MySpacerColumn(height = 8.dp)
 
             Text(
-                text = "It may took a while"
+                text = stringResource(id = R.string.it_mat_took_a_while),
+                style = MaterialTheme.typography.bodyMedium
             )
 
             MySpacerColumn(height = 40.dp)
