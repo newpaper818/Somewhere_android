44e630e98cbd7058d35520d799f6918c31c28e90
sejong <snm40109@gmail.com>
2024-10-18 17:41:22 +0900

    ♻️ date color list fix

--- core/ui/designsystem/src/main/java/com/newpaper/somewhere/core/designsystem/theme/Color.kt
+++ core/ui/designsystem/src/main/java/com/newpaper/somewhere/core/designsystem/theme/Color.kt
@@ -1,6 +1,22 @@
 package com.newpaper.somewhere.core.designsystem.theme
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.newpaper.somewhere.core.model.data.MyColor
 
 //Color.kt
@@ -170,43 +186,76 @@ const val whiteInt = 0xffffffff.toInt()
 
 
 val dateColorList = listOf(
-    MyColor(0xFF493cfa.toInt()),
-    MyColor(0xFF7168e8.toInt()),
-    MyColor(0xFF000000.toInt()),
-    MyColor(0xFFffffff.toInt(), blackInt),
 
-    MyColor(0xFF7f7f7f.toInt(), blackInt),
-    MyColor(0xFFc3c3c3.toInt(), blackInt),
-    MyColor(0xFF880015.toInt()),
+    MyColor(0xFF493cfa.toInt()),
+    MyColor(0xFF22b14c.toInt(), blackInt),
+    MyColor(0xFFff7f27.toInt(), blackInt),
+    MyColor(0xFF732bf5.toInt()),
+
+    MyColor(0xFFffaec9.toInt(), blackInt),
+    MyColor(0xFFed1c24.toInt()),
+    MyColor(0xFF00a2e8.toInt(), blackInt),
+    MyColor(0xFFa349a4.toInt()),
+
+    MyColor(0xFF0023f5.toInt()),
+    MyColor(0xFF73fbfd.toInt(), blackInt),
+    MyColor(0xFF7168e8.toInt()),
     MyColor(0xFFb97a57.toInt()),
 
-    MyColor(0xFFed1c24.toInt()),
-    MyColor(0xFFffaec9.toInt(), blackInt),
-    MyColor(0xFFff7f27.toInt(), blackInt),
-    MyColor(0xFFffc90e.toInt(), blackInt),
-
-    MyColor(0xFFfff200.toInt(), blackInt),
-    MyColor(0xFFefe4b0.toInt(), blackInt),
-    MyColor(0xFF22b14c.toInt(), blackInt),
-    MyColor(0xFFb5e61d.toInt(), blackInt),
-
-    MyColor(0xFF00a2e8.toInt(), blackInt),
+    MyColor(0xFF880015.toInt()),
     MyColor(0xFF99d9ea.toInt(), blackInt),
+    MyColor(0xFFfff200.toInt(), blackInt),
     MyColor(0xFF3f48cc.toInt()),
-    MyColor(0xFF7092be.toInt(), blackInt),
-
-    MyColor(0xFFa349a4.toInt()),
-    MyColor(0xFFc8bfe7.toInt(), blackInt),
-    MyColor(0xFF732bf5.toInt()),
-    MyColor(0xFF3a083e.toInt()),
 
     MyColor(0xFF75fa8d.toInt(), blackInt),
-    MyColor(0xFF73fbfd.toInt(), blackInt),
-    MyColor(0xFF3a0603.toInt()),
-    MyColor(0xFF183e0c.toInt()),
-
-    MyColor(0xFF817f26.toInt()),
-    MyColor(0xFF0023f5.toInt()),
-    MyColor(0xFF75163f.toInt()),
     MyColor(0xFF7f82bb.toInt(), blackInt),
+    MyColor(0xFF3a083e.toInt()),
+    MyColor(0xFF7092be.toInt(), blackInt),
+
+    MyColor(0xFF3a0603.toInt()),
+    MyColor(0xFFffc90e.toInt(), blackInt),
+    MyColor(0xFF183e0c.toInt()),
+    MyColor(0xFFefe4b0.toInt(), blackInt),
+
+    MyColor(0xFFb5e61d.toInt(), blackInt),
+    MyColor(0xFFc8bfe7.toInt(), blackInt),
+    MyColor(0xFF817f26.toInt()),
+    MyColor(0xFF75163f.toInt()),
+
+    MyColor(0xFF000000.toInt()),
+    MyColor(0xFF7f7f7f.toInt(), blackInt),
+    MyColor(0xFFc3c3c3.toInt(), blackInt),
+    MyColor(0xFFffffff.toInt(), blackInt),
 )
+
+@Preview
+@Composable
+private fun DateColorPreview(
+
+){
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(4),
+        modifier = Modifier.width(140.dp)
+    ) {
+        itemsIndexed(dateColorList){ index, myColor ->
+            Box(
+                modifier = Modifier.size(35.dp),
+                contentAlignment = Alignment.Center
+            ){
+                Box(
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .background(Color(myColor.color))
+                        .size(30.dp),
+                    contentAlignment = Alignment.Center
+                ){
+                    Text(
+                        text = (index+1).toString(),
+                        color = Color(myColor.onColor),
+                        fontSize = 12.sp
+                    )
+                }
+            }
+        }
+    }
+}
