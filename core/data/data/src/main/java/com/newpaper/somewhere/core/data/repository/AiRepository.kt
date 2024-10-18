b2458e9bf8e91e3e626386f7233008cada888963
sejong <snm40109@gmail.com>
2024-10-18 16:07:00 +0900

    ✨ add location on ai created trip

--- core/data/data/src/main/java/com/newpaper/somewhere/core/data/repository/AiRepository.kt
+++ core/data/data/src/main/java/com/newpaper/somewhere/core/data/repository/AiRepository.kt
@@ -1,14 +1,21 @@
 package com.newpaper.somewhere.core.data.repository
 
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.libraries.places.api.model.Place
 import com.newpaper.gemini_ai.AiRemoteDataSource
+import com.newpaper.somewhere.core.data.moshi.SerializationDataSource
 import com.newpaper.somewhere.core.google_map_places.dataSource.PlacesRemoteDataSource
+import com.newpaper.somewhere.core.model.tripData.Date
+import com.newpaper.somewhere.core.model.tripData.Spot
+import com.newpaper.somewhere.core.model.tripData.Trip
 import java.time.Duration
 import java.time.LocalDate
 import javax.inject.Inject
 
 class AiRepository @Inject constructor(
     private val aiRemoteDataSource: AiRemoteDataSource,
-    private val placesRemoteDataSource: PlacesRemoteDataSource
+    private val placesRemoteDataSource: PlacesRemoteDataSource,
+    private val serializationDataSource: SerializationDataSource
 ) {
     suspend fun getAiCreatedTrip(
         city: String,
@@ -17,8 +24,8 @@ class AiRepository @Inject constructor(
         tripWith: String,
         tripType: String,
         language: String
-    ): String? {
-        //get places from ai
+    ): Trip? {
+        //get places from ai / return: [N seoul tower, lotte tower ...]
         val list = aiRemoteDataSource.getRecommendSpots(
             city = city,
             tripDays = Duration.between(startDate.atStartOfDay(), endDate.atStartOfDay()).toDays().toInt() + 1,
@@ -26,22 +33,86 @@ class AiRepository @Inject constructor(
             tripType = tripType
         )
 
+        if (list == null)
+            return null
+
         //get places info from Google places
-        return if (list != null) {
-            val places = placesRemoteDataSource.getPlacesInfo(list)
+        val places = placesRemoteDataSource.getPlacesInfo(list)
 
-            //get trip plans from ai
-            if (places != null){
-                aiRemoteDataSource.getTripPlan(
-                    places = places,
-                    city = city,
-                    tripDate = "$startDate ~ $endDate",
-                    tripWith = tripWith,
-                    tripType = tripType,
-                    language = language
+        if (places == null)
+            return null
+
+        //get trip plans from ai
+        val aiCreatedTripJson = aiRemoteDataSource.getTripPlan(
+            places = places,
+            city = city,
+            tripDate = "$startDate ~ $endDate",
+            tripWith = tripWith,
+            tripType = tripType,
+            language = language
+        )
+
+        if (aiCreatedTripJson == null)
+            return null
+
+        //convert JSON -> Trip
+        val aiCreatedTrip = serializationDataSource.jsonToTrip(aiCreatedTripJson)
+
+        if (aiCreatedTrip == null)
+            return null
+
+        //add location
+        val tripWithLocation = addLocation(
+            trip = aiCreatedTrip,
+            places = places
+        )
+
+        return tripWithLocation
+    }
+
+    private fun addLocation(
+        trip: Trip,
+        places: List<Place>
+    ): Trip {
+        val dateList = trip.dateList
+
+        val newDateList = mutableListOf<Date>()
+        dateList.forEach { date ->
+            val spotList = date.spotList
+            val newSpotList = mutableListOf<Spot>()
+
+            spotList.forEach{ spot ->
+                val newSpot = spot.copy(
+                    location = getLatLngFromPlaceId(
+                        placeId = spot.googleMapsPlacesId,
+                        places = places
+                    ),
+                    zoomLevel = 15f
                 )
-            } else null
+                newSpotList.add(newSpot)
+            }
 
-        } else null
+            val newDate = date.copy(
+                spotList = newSpotList
+            )
+            newDateList.add(newDate)
+        }
+
+        val newTrip = trip.copy(
+            dateList = newDateList
+        )
+
+        return newTrip
+    }
+
+    private fun getLatLngFromPlaceId(
+        placeId: String?,
+        places: List<Place>
+    ): LatLng? {
+        val place = places.find {
+            it.id == placeId
+        }
+
+        return place?.location
     }
 }
