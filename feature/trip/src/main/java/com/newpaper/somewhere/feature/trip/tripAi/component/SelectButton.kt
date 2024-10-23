0a663bd80e7daeaa1bb8c60c28dd04deac46d61e
sejong <snm40109@gmail.com>
2024-10-23 10:35:05 +0900

    ♿️ MySegmentedButtons improve accessibility - add state description, onClickLabel

--- feature/trip/src/main/java/com/newpaper/somewhere/feature/trip/tripAi/component/SelectButton.kt
+++ feature/trip/src/main/java/com/newpaper/somewhere/feature/trip/tripAi/component/SelectButton.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.stateDescription
 import androidx.compose.ui.unit.dp
