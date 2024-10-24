c45a2f39026a5672de8b004cb8a795ea4d68bdbc
sejong <snm40109@gmail.com>
2024-10-24 10:50:17 +0900

    ♿️ VersionCard, DeveloperCard mergeDescendants

--- core/ui/ui/src/main/java/com/newpaper/somewhere/core/ui/card/VersionCard.kt
+++ core/ui/ui/src/main/java/com/newpaper/somewhere/core/ui/card/VersionCard.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import com.newpaper.somewhere.core.designsystem.component.button.UpdateButton
@@ -39,7 +40,7 @@ fun VersionCard(
 
 
     MyCard(
-        modifier = modifier
+        modifier = modifier.semantics(mergeDescendants = true) { }
     ) {
         Row(
             modifier = Modifier.fillMaxWidth().padding(16.dp, 14.dp),
