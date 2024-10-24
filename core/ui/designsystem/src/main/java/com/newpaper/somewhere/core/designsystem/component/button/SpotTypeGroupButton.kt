a81bc94747572c04c288d4d8f8bc59170b8da6dc
sejong <snm40109@gmail.com>
2024-10-24 15:03:50 +0900

    ♿️ DateScreen semantics

--- core/ui/designsystem/src/main/java/com/newpaper/somewhere/core/designsystem/component/button/SpotTypeGroupButton.kt
+++ core/ui/designsystem/src/main/java/com/newpaper/somewhere/core/designsystem/component/button/SpotTypeGroupButton.kt
@@ -20,12 +20,16 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.onClick
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.stateDescription
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.newpaper.somewhere.core.designsystem.theme.SomewhereTheme
 import com.newpaper.somewhere.core.model.enums.SpotTypeGroup
 import com.newpaper.somewhere.core.model.tripData.Date
+import com.newpaper.somewhere.core.ui.designsystem.R
 import com.newpaper.somewhere.core.utils.convert.getSpotTypeGroupCount
 
 
@@ -91,11 +95,25 @@ fun SpotTypeGroupCard(
         }
         else    MaterialTheme.typography.labelLarge.copy(color = MaterialTheme.colorScheme.onSurfaceVariant)
 
+    val selectedText = stringResource(id = R.string.selected)
+    val notSelectedText = stringResource(id = R.string.not_selected)
+    val toggle = stringResource(id = R.string.toggle)
+
     Button(
         colors = ButtonDefaults.buttonColors(containerColor = buttonColor),
         border = border,
         contentPadding = PaddingValues(12.dp, 0.dp, 12.dp, 1.dp),
-        onClick = { onCardClicked(spotTypeGroup)}
+        onClick = { onCardClicked(spotTypeGroup)},
+        modifier = Modifier.semantics {
+            stateDescription = if (selected) selectedText else notSelectedText
+            onClick(
+                label = toggle,
+                action = {
+                    onCardClicked(spotTypeGroup)
+                    true
+                }
+            )
+        }
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically
