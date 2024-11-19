e95876bf1d18f5381d77cd396a34baada2c9134e
sejong <snm40109@gmail.com>
2024-11-19 11:02:32 +0900

    🔖 v 1.10.2 (49) release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.newpaper.somewhere"
         minSdk = 26
         targetSdk = 34
-        versionCode = 48
-        versionName = "1.10.1"
+        versionCode = 49
+        versionName = "1.10.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
