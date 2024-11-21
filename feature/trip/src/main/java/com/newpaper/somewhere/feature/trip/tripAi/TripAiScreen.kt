aa49f3f84ee6f61acb343997c0f4b6872afe12b1
sejong <snm40109@gmail.com>
2024-11-21 10:59:03 +0900

    ♿️ TripAiScreen CloseButton add semantics

--- feature/trip/src/main/java/com/newpaper/somewhere/feature/trip/tripAi/TripAiScreen.kt
+++ feature/trip/src/main/java/com/newpaper/somewhere/feature/trip/tripAi/TripAiScreen.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.util.Log
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.displayCutoutPadding
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +15,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -41,6 +41,7 @@ import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.FullScreenContentCallback
 import com.google.android.gms.ads.rewardedinterstitial.RewardedInterstitialAd
+import com.newpaper.somewhere.core.designsystem.component.utils.MyPlainTooltipBox
 import com.newpaper.somewhere.core.designsystem.icon.DisplayIcon
 import com.newpaper.somewhere.core.designsystem.icon.TopAppBarIcon
 import com.newpaper.somewhere.core.model.data.DateTimeFormat
@@ -487,18 +488,28 @@ private fun CloseButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ){
+    val close = stringResource(id = R.string.close)
+
+
+
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
-        Box(
-            contentAlignment = Alignment.Center,
+        MyPlainTooltipBox(
+            tooltipText = close,
             modifier = modifier
                 .size(40.dp)
                 .clip(CircleShape)
                 .background(MaterialTheme.colorScheme.surfaceDim.copy(alpha = 0.8f))
-                .clickable { onClick() }
         ) {
-            DisplayIcon(icon = TopAppBarIcon.close)
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                IconButton(onClick = onClick) {
+                    DisplayIcon(icon = TopAppBarIcon.close)
+                }
+            }
         }
     }
 }
