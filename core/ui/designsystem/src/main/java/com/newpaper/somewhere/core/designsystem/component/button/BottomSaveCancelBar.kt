bcf62dd63b0ba80dd7addd66b307bdd96da0e1a9
sejong <snm40109@gmail.com>
2025-08-28 19:51:19 +0900

    💄 AnimatedBottomSaveCancelButtons bottom padding fix

--- core/ui/designsystem/src/main/java/com/newpaper/somewhere/core/designsystem/component/button/BottomSaveCancelBar.kt
+++ core/ui/designsystem/src/main/java/com/newpaper/somewhere/core/designsystem/component/button/BottomSaveCancelBar.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
@@ -38,7 +39,8 @@ fun AnimatedBottomSaveCancelButtons(
     onClickSave: () -> Unit,
     modifier: Modifier = Modifier,
     saveEnabled: Boolean = true,
-    use2PanesAndSpotScreen: Boolean = false
+    use2PanesAndSpotScreen: Boolean = false,
+    useBottomNavBar: Boolean = false
 ){
     AnimatedVisibility(
         visible = visible,
@@ -80,6 +82,14 @@ fun AnimatedBottomSaveCancelButtons(
                         .padding(10.dp, 2.dp, 10.dp, 10.dp),
                     positiveEnabled = saveEnabled,
                 )
+
+                if (!useBottomNavBar)
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(MaterialTheme.colorScheme.background)
+                            .navigationBarsPadding()
+                    )
             }
         }
     }
