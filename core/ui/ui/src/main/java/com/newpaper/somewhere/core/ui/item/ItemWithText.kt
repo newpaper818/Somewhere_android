43e53f5404d492883a1678378d8beebe4a5052c9
sejong <snm40109@gmail.com>
2026-01-13 11:37:43 +0900

    💄 update ItemWithText - add titleText

--- core/ui/ui/src/main/java/com/newpaper/somewhere/core/ui/item/ItemWithText.kt
+++ core/ui/ui/src/main/java/com/newpaper/somewhere/core/ui/item/ItemWithText.kt
@@ -6,7 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
@@ -19,6 +19,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import com.newpaper.somewhere.core.designsystem.component.utils.ClickableBox
+import com.newpaper.somewhere.core.designsystem.component.utils.MySpacerColumn
 import com.newpaper.somewhere.core.designsystem.component.utils.MySpacerRow
 import com.newpaper.somewhere.core.designsystem.icon.DisplayIcon
 import com.newpaper.somewhere.core.designsystem.icon.MyIcon
@@ -26,16 +27,31 @@ import com.newpaper.somewhere.core.designsystem.icon.MyIcons
 import com.newpaper.somewhere.core.designsystem.theme.SomewhereTheme
 import com.newpaper.somewhere.core.utils.listItemHeight
 
+
+/**
+ * use with [ListGroupCard]
+ *
+ * @param isSelected show gray background when true
+ * @param icon icon at left side
+ * @param titleText top small gray text
+ * @param text main text
+ * @param subText gray text at right side
+ * @param iconRight icon at right side
+ * @param onItemClick when click item
+ */
 @Composable
 fun ItemWithText(
     isSelected: Boolean = false,
     icon: MyIcon? = null,
+    titleText: String? = null,
     text: String?= null,
     subText: String? = null,
 
-    isOpenInNew: Boolean = false,
+    iconRight: MyIcon? = null,
+
     onItemClick: (() -> Unit)? = null
 ){
+    val titleTextStyle = MaterialTheme.typography.bodySmall.copy(color = MaterialTheme.colorScheme.onSurfaceVariant)
     val textStyle = MaterialTheme.typography.bodyLarge
     val subTextStyle = MaterialTheme.typography.bodyMedium.copy(color = MaterialTheme.colorScheme.onSurfaceVariant)
 
@@ -53,27 +69,42 @@ fun ItemWithText(
                 verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(listItemHeight)
-                    .padding(16.dp, 0.dp)
+                    .heightIn(min = listItemHeight)
+                    .padding(16.dp, 14.dp)
             ) {
-                if (icon != null) {
-                    DisplayIcon(icon = icon)
+                Column {
+                    if (titleText != null) {
+                        Text(
+                            text = titleText,
+                            style = titleTextStyle
+                        )
 
-                    MySpacerRow(width = 10.dp)
+                        MySpacerColumn(height = 8.dp)
+                    }
+
+                    Row {
+                        if (icon != null) {
+                            DisplayIcon(icon = icon)
+
+                            MySpacerRow(width = 10.dp)
+                        }
+
+                        if (text != null) {
+                            Text(
+                                text = text,
+                                style = textStyle
+                            )
+
+                            MySpacerRow(width = 8.dp)
+                        }
+                    }
                 }
 
-                if (text != null) {
-                    Text(
-                        text = text,
-                        style = textStyle
-                    )
-
-                    MySpacerRow(width = 8.dp)
+                if (subText != null || iconRight != null) {
+                    Spacer(modifier = Modifier.weight(1f))
                 }
 
                 if (subText != null) {
-                    Spacer(modifier = Modifier.weight(1f))
-
                     Text(
                         text = subText,
                         style = subTextStyle,
@@ -82,10 +113,10 @@ fun ItemWithText(
                     )
                 }
 
-                if (isOpenInNew) {
-                    Spacer(modifier = Modifier.weight(1f))
+                if (iconRight != null) {
+                    MySpacerRow(width = 8.dp)
 
-                    DisplayIcon(icon = MyIcons.openInNew)
+                    DisplayIcon(icon = iconRight)
                 }
             }
         }
@@ -117,7 +148,7 @@ private fun Preview(){
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.surface)
                 .padding(16.dp)
-                .width(250.dp)
+                .width(300.dp)
         ) {
             ListGroupCard {
                 ItemWithText(
@@ -144,9 +175,56 @@ private fun Preview(){
                 ItemWithText(
                     text = "item",
                     subText = "subtext",
-                    isOpenInNew = true
+                    iconRight = MyIcons.openInNew
+                )
+
+                ItemDivider()
+
+                ItemWithText(
+                    icon = MyIcons.time,
+                    text = "item",
+                    subText = "subtext",
+                    iconRight = MyIcons.openInNew
+                )
+
+                ItemDivider()
+
+                ItemWithText(
+                    titleText = "title",
+                    text = "item",
+                    iconRight = MyIcons.sendEmail
+                )
+
+                ItemDivider()
+
+                ItemWithText(
+                    titleText = "title",
+                    icon = MyIcons.time,
+                    text = "item",
+                    iconRight = MyIcons.openInNew
                 )
             }
+        }
+    }
+}
+
+@Composable
+@PreviewLightDark
+private fun PreviewDont(){
+    SomewhereTheme {
+        Box(
+            contentAlignment = Alignment.BottomEnd,
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.surface)
+                .padding(16.dp)
+                .width(300.dp)
+        ) {
+            ItemWithText(
+                titleText = "title",
+                icon = MyIcons.time,
+                text = "item",
+                iconRight = MyIcons.openInNew
+            )
         }
     }
 }
