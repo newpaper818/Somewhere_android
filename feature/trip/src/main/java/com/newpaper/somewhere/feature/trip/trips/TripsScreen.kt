a8c3bc82b05dbcfa42c3b9d80a2a1a85e11ac6f8
sejong <snm40109@gmail.com>
2026-01-20 23:33:15 +0900

    👔 apply Baseline Profiles

--- feature/trip/src/main/java/com/newpaper/somewhere/feature/trip/trips/TripsScreen.kt
+++ feature/trip/src/main/java/com/newpaper/somewhere/feature/trip/trips/TripsScreen.kt
@@ -1,6 +1,5 @@
 package com.newpaper.somewhere.feature.trip.trips
 
-import android.util.Log
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
@@ -30,6 +29,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -599,6 +599,7 @@ private fun TripsScreen(
                                     .fillMaxWidth()
                                     .padding(horizontal = 16.dp)
                                     .wrapContentHeight(Alignment.Bottom)
+                                    .testTag("my_trips")
                             )
                         }
                     }
