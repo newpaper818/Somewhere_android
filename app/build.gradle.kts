8a7a634e498798f5a265decb122f61280bac78c2
sejong <snm40109@gmail.com>
2026-06-08 19:20:08 +0900

    🚀 v 2.4.1 (77) release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.newpaper.somewhere"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 76
-        versionName = "2.4.0"
+        versionCode = 77
+        versionName = "2.4.1-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
